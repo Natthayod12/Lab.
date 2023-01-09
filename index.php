7dd3afc287898408c67dd12a7212383349b7c818
Natthayod <Natthayod.u@ku.th>
2023-01-08 21:31:11 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <form action="show.php" method="POST"> 
+        what is your name:<input type="text" name="name"><br>
+        waht is your age:<input type="text" name="age"><br>
+    
+    <input type="Submit">
+
+
+</body>
+</html>
---
+++ show.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+    <?php
+        $name = $_POST["name"];
+        $age = $_POST["age"];
+
+        echo "name:".$name."<br>";
+        echo "age".$age."<br>";
+
+    ?>
+    
+</body>
+</html>
